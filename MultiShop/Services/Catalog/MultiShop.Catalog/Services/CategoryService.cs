796619d6c36ea5e90bff3b02e9b7da36eaa7fabe
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2024-06-01 02:58:02 +0300

    Category service consturctor added

--- MultiShop/Services/Catalog/MultiShop.Catalog/Services/CategoryService.cs
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Services/CategoryService.cs
@@ -2,6 +2,7 @@
 using MongoDB.Driver;
 using MultiShop.Catalog.Dtos.CategoryDtos;
 using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Settings;
 
 namespace MultiShop.Catalog.Services
 {
@@ -9,6 +10,22 @@ namespace MultiShop.Catalog.Services
     {
         private readonly IMongoCollection<Category> _categoryCollection;
         private readonly IMapper _mapper;
+
+        public CategoryService(IMapper mapper, IDatabaseSettings _databaseSettings)
+        {
+            //MongoClient sınıfının bir örneği oluşturulur.
+            //Bu, MongoDB sunucusuna bağlanmak için kullanılır ve
+            //bağlantı dizesi _databaseSettings.ConnectionString parametresiyle sağlanır.
+            var client = new MongoClient(_databaseSettings.ConnectionString);
+
+            //GetDatabase yöntemi kullanılarak belirtilen adı
+            //(_databaseSettings.DatabaseName) taşıyan bir veritabanı nesnesi elde edilir.
+            var database = client.GetDatabase(_databaseSettings.DatabaseName);
+
+
+            _categoryCollection = database.GetCollection<Category>(_databaseSettings.CategoryCollectionName);
+            _mapper = mapper;
+        }
         public Task CreateCategoryAsync(CreateCategoryDto createCategoryDto)
         {
             throw new NotImplementedException();
