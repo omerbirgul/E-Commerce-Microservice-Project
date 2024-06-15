3bf9013ded97f34a9680dbab707830c516ed117f
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2024-06-15 23:54:23 +0300

    ProductImagesController added

---
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Controllers/ProductImagesController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.ProductImageDtos;
+using MultiShop.Catalog.Services.ProductImageServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductImagesController : ControllerBase
+    {
+        private readonly IProductImageService _productImageService;
+
+        public ProductImagesController(IProductImageService productImageService)
+        {
+            _productImageService = productImageService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ProductImageList()
+        {
+            var values = await _productImageService.GetAllProductImageAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductImageById(string id)
+        {
+            var values = _productImageService.GetByIdProductImageAsync(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProductImage(CreateProductImageDto createProductImageDto)
+        {
+            await _productImageService.CreateProductImageAsync(createProductImageDto);
+            return Ok("ProductImage created succesfully");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProductImage(string id)
+        {
+            await _productImageService.DeleteProductImageAsync(id);
+            return Ok("ProductImage deleted succesfully");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProductImage(UpdateProductImageDto updateProductImageDto)
+        {
+            await _productImageService.UpdateProductImageAsync(updateProductImageDto);
+            return Ok("ProductImage updated succesfully");
+        }
+    }
+}
