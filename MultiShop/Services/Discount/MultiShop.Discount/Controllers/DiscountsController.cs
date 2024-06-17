e0abb2cdbcc1d895d77edd14b484427517630453
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2024-06-18 02:15:34 +0300

    DiscountsController added

---
+++ MultiShop/Services/Discount/MultiShop.Discount/Controllers/DiscountsController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Discount.Dtos;
+using MultiShop.Discount.Services.DiscountServices;
+
+namespace MultiShop.Discount.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountsController : ControllerBase
+    {
+        private readonly IDiscountService _discountService;
+
+        public DiscountsController(IDiscountService discountService)
+        {
+            _discountService = discountService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DiscountCouponList()
+        {
+            var values = await _discountService.GetAllCouponAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetDiscountCouponById(int id)
+        {
+            var values = await _discountService.GetByIdCouponAsync(id);
+            return Ok(values);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteDiscountCoupon(int id)
+        {
+            await _discountService.DeleteCouponAsync(id);
+            return Ok("Coupon deleted successfully");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateDiscountCoupon(CreateCouponDto createCouponDto)
+        {
+            await _discountService.CreateCouponAsync(createCouponDto);
+            return Ok("Coupon created successfully");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdatDiscountCoupon(UpdateCouponDto updateCouponDto)
+        {
+            await _discountService.UpdateCouponAsync(updateCouponDto);
+            return Ok("Coupon updated successfully");
+        }
+
+    }
+}
--- MultiShop/Services/Discount/MultiShop.Discount/Services/DiscountServices/DiscountService.cs
+++ MultiShop/Services/Discount/MultiShop.Discount/Services/DiscountServices/DiscountService.cs
@@ -76,3 +76,4 @@ namespace MultiShop.Discount.Services.DiscountServices
         }
     }
 }
+   
