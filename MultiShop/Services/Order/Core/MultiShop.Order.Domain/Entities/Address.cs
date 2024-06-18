60c14aa808d075504dc51c052d0d4cb63518c329
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2024-06-18 15:26:33 +0300

    Order Domain Layer

--- MultiShop/Services/Order/Core/MultiShop.Order.Domain/Entities/Address.cs
+++ MultiShop/Services/Order/Core/MultiShop.Order.Domain/Entities/Address.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+   
 namespace MultiShop.Order.Domain.Entities
 {
-    internal class Address
+    public class Address
     {
+        public int AddressId { get; set; }
+        public string UserId { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }
+        public string Detail { get; set; }
     }
 }
