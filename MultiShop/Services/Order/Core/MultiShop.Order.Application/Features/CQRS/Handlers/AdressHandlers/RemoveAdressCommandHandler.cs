8a73d28f33a932c83c5c4a3ac060a535f102240b
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2024-06-18 19:59:00 +0300

    CQRS Update and Delete AddressHandlers added

---
+++ MultiShop/Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AdressHandlers/RemoveAdressCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Commands.AdressCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.AdressHandlers
+{
+    public class RemoveAdressCommandHandler
+    {
+        private readonly IRepository<Address> _repository;
+
+        public RemoveAdressCommandHandler(IRepository<Address> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(RemoveAdressCommand commandHandler)
+        {
+            var values = await _repository.GetByIdAsync(commandHandler.Id);
+            await _repository.DeleteAsync(values);
+        }
+    }
+}
---
+++ MultiShop/Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AdressHandlers/UpdateAddressCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Commands.AdressCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.AdressHandlers
+{
+    public class UpdateAddressCommandHandler
+    {
+        private readonly IRepository<Address> _repository;
+
+        public UpdateAddressCommandHandler(IRepository<Address> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateAdressCommand updateAdressCommand)
+        {
+            var values = await _repository.GetByIdAsync(updateAdressCommand.AddressId);
+            values.Detail = updateAdressCommand.Detail;
+            values.City = updateAdressCommand.City;
+            values.District = updateAdressCommand.District;
+            values.UserId = updateAdressCommand.UserId;
+            await _repository.UpdateAsync(values);
+        }
+    }
+}
