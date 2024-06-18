c51a5a4e5ac9dd97482d9ad9dd97476bd39b1090
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2024-06-18 19:04:41 +0300

    CQRS GetAddressQueryResult added

---
+++ MultiShop/Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AdressHandlers/GetAddressQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Results.AdressResults;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.AdressHandlers
+{
+    public class GetAddressQueryHandler
+    {
+        private readonly IRepository<Address> _repository;
+
+        public GetAddressQueryHandler(IRepository<Address> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetAdressQueryResult>> Handle()
+        {
+            var values = await _repository.GetAllAsync();
+            return values.Select(x => new GetAdressQueryResult
+            {
+                AddressId = x.AddressId,
+                City = x.City,
+                Detail = x.Detail,
+                District = x.District,
+                UserId = x.UserId
+            }).ToList();
+        }
+    }
+}
