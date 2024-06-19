d4007e04465cc6fa84875cbc9c470d05a70faa2a
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2024-06-19 16:32:45 +0300

    CQRS registration processes

--- MultiShop/Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
+++ MultiShop/Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
@@ -1,4 +1,19 @@
+using MultiShop.Order.Application.Features.CQRS.Handlers.AdressHandlers;
+using MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<GetAddressQueryHandler>();
+builder.Services.AddScoped<GetAddressByIdQueryHandler>();
+builder.Services.AddScoped<CreateAdressCommandHandler>();
+builder.Services.AddScoped<UpdateAddressCommandHandler>();
+builder.Services.AddScoped<RemoveAdressCommandHandler>();
+
+builder.Services.AddScoped<GetOrderDetailQueryHandler>();
+builder.Services.AddScoped<GetOrderDetailByIdQueryHandler>();
+builder.Services.AddScoped<CreateOrderDetailCommandHandler>();
+builder.Services.AddScoped<UpdateOrderDetailCommandHandler>();
+builder.Services.AddScoped<RemoveOrderDetailCommandHandler>();
 
 // Add services to the container.
 
