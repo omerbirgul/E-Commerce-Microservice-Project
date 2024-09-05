17c2b29ffdc31982f1339bdf949b48472f3fa488
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2024-09-05 20:03:43 +0300

    CargoService Dtos and mapping added

---
+++ MultiShop/Services/Cargo/MultiShop.Cargo.WebApi/Mapping/GeneralMapping.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using MultiShop.Cargo.DtoLayer.CargoCompanyDtos;
+using MultiShop.Cargo.DtoLayer.CargoCustomerDtos;
+using MultiShop.Cargo.DtoLayer.CargoDetailDtos;
+using MultiShop.Cargo.DtoLayer.CargoOperationDtos;
+using MultiShop.Cargo.EntityLayer.Concrete;
+
+namespace MultiShop.Cargo.WebApi.Mapping
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<CargoCompany, CreateCargoCompanyDto>().ReverseMap();
+            CreateMap<CargoCompany, UpdateCargoCompanyDto>().ReverseMap();
+
+            CreateMap<CargoCustomer, CreateCargoCustomerDto>().ReverseMap();
+            CreateMap<CargoCustomer, UpdateCargoCustomerDto>().ReverseMap();
+
+            CreateMap<CargoDetail, CreateCargoDetailDto>().ReverseMap();
+            CreateMap<CargoDetail, UpdateCargoDetailDto>().ReverseMap();
+
+            CreateMap<CargoOperation, CreateCargoOperationDto>().ReverseMap();
+            CreateMap<CargoOperation, UpdateCargoOperationDto>().ReverseMap();
+        }
+
+    }
+}
