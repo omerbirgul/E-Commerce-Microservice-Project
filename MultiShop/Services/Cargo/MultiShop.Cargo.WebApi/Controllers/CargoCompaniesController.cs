3c88ed2fb7010b9e2e9dfb43c4fe61330a9be820
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2024-09-05 20:23:25 +0300

    CargoService  CargoCompaniesController added

---
+++ MultiShop/Services/Cargo/MultiShop.Cargo.WebApi/Controllers/CargoCompaniesController.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Cargo.BusinessLayer.Abstract;
+using MultiShop.Cargo.DtoLayer.CargoCompanyDtos;
+using MultiShop.Cargo.EntityLayer.Concrete;
+
+namespace MultiShop.Cargo.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CargoCompaniesController : ControllerBase
+    {
+        private readonly ICargoCompanyService _cargoCompanyService;
+        private readonly IMapper _mapper;
+
+        public CargoCompaniesController(ICargoCompanyService cargoCompanyService, IMapper mapper)
+        {
+            _cargoCompanyService = cargoCompanyService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult CargoCompanyList()
+        {
+            var values = _cargoCompanyService.TGetAll();
+            return Ok(values);
+        }
+
+        [HttpGet]
+        public IActionResult GetCargoCompaniesById(int id)
+        {
+            var values = _cargoCompanyService.TGetById(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCargoCompany(CreateCargoCompanyDto createCargoCompanyDto)
+        {
+            var values = _mapper.Map<CargoCompany>(createCargoCompanyDto);
+            _cargoCompanyService.TInsert(values);
+            return Ok("Cargo company created successfully");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCargoCompany(UpdateCargoCompanyDto updateCargoCompanyDto)
+        {
+            var values = _mapper.Map<CargoCompany>(updateCargoCompanyDto);
+            _cargoCompanyService.TUpdate(values);
+            return Ok("Cargo company updated successfully");
+        }
+
+        [HttpDelete]
+        public IActionResult RemoveCargoCompany(int id)
+        {
+            _cargoCompanyService.TDelete(id);
+            return Ok("Cargo company deleted successfully");
+        }
+
+    }
+}
