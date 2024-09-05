294be7318a2aa5bde04e6086149ac1913a87014f
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2024-09-05 20:34:53 +0300

    CargoService CargoCustomersController added

--- MultiShop/Services/Cargo/MultiShop.Cargo.WebApi/Controllers/CargoCustomersController.cs
+++ MultiShop/Services/Cargo/MultiShop.Cargo.WebApi/Controllers/CargoCustomersController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MultiShop.Cargo.BusinessLayer.Abstract;
+using MultiShop.Cargo.DtoLayer.CargoCustomerDtos;
+using MultiShop.Cargo.EntityLayer.Concrete;
 
 namespace MultiShop.Cargo.WebApi.Controllers
 {
@@ -7,5 +11,50 @@ namespace MultiShop.Cargo.WebApi.Controllers
     [ApiController]
     public class CargoCustomersController : ControllerBase
     {
+        private readonly ICargoCustomerService _cargoCustomerService;
+        private readonly IMapper _mapper;
+
+        public CargoCustomersController(ICargoCustomerService cargoCustomerService, IMapper mapper)
+        {
+            _cargoCustomerService = cargoCustomerService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult CargoCustomerList()
+        {
+            var values = _cargoCustomerService.TGetAll();
+            return Ok(values);
+        }
+
+        [HttpGet]
+        public IActionResult GetCargoCustomerById(int id)
+        {
+            var values = _cargoCustomerService.TGetById(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCargoCustomer(CreateCargoCustomerDto createCargoCustomerDto)
+        {
+            var values = _mapper.Map<CargoCustomer>(createCargoCustomerDto);
+            _cargoCustomerService.TInsert(values);
+            return Ok("Cargo customer created successfully");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCargoCustomer(UpdateCargoCustomerDto updateCargoCustomerDto)
+        {
+            var values = _mapper.Map<CargoCustomer>(updateCargoCustomerDto);
+            _cargoCustomerService.TUpdate(values);
+            return Ok("Cargo updated successfully");
+        }
+
+        [HttpDelete]
+        public IActionResult RemoveCargoCustomer(int id)
+        {
+            _cargoCustomerService.TDelete(id);
+            return Ok("Cargo customer deleted successfully");
+        }
     }
 }
