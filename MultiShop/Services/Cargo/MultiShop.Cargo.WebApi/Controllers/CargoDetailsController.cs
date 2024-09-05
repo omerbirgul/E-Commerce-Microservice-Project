cff92d549ce3623dd0b94896f852ee793b4a5937
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2024-09-05 20:44:01 +0300

    CargoService CargoDetailsController added

---
+++ MultiShop/Services/Cargo/MultiShop.Cargo.WebApi/Controllers/CargoDetailsController.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Cargo.BusinessLayer.Abstract;
+using MultiShop.Cargo.DtoLayer.CargoDetailDtos;
+using MultiShop.Cargo.EntityLayer.Concrete;
+
+namespace MultiShop.Cargo.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CargoDetailsController : ControllerBase
+    {
+        private readonly ICargoDetailService _cargoDetailService;
+        private readonly IMapper _mapper;
+
+        public CargoDetailsController(ICargoDetailService cargoDetailService, IMapper mapper)
+        {
+            _cargoDetailService = cargoDetailService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult CargoDetailList()
+        {
+            var values = _cargoDetailService.TGetAll();
+            return Ok(values);
+        }
+
+        [HttpGet]
+        public IActionResult GetCargoDetailById(int id)
+        {
+            var values = _cargoDetailService.TGetById(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCargoDetail(CreateCargoDetailDto createCargoDetailDto)
+        {
+            var values = _mapper.Map<CargoDetail>(createCargoDetailDto);
+            _cargoDetailService.TInsert(values);
+            return Ok("Cargo detail created successfully");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCargoDetail(UpdateCargoDetailDto updateCargoDetailDto)
+        {
+            var values = _mapper.Map<CargoDetail>(updateCargoDetailDto);
+            _cargoDetailService.TUpdate(values);
+            return Ok("Cargo detail updated successfully");
+        }
+
+        [HttpDelete]
+        public IActionResult RemoveCargoDetail(int id)
+        {
+            _cargoDetailService.TDelete(id);
+            return Ok("Cargo detail deleted successfully");
+        }
+    }
+}
