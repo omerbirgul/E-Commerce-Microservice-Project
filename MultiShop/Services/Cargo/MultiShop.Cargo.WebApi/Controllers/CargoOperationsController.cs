8af25dc0c37d8e24b47dc2cf2d754b91b9c48b62
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2024-09-05 20:54:15 +0300

    CargoService CargoOperationsController added

---
+++ MultiShop/Services/Cargo/MultiShop.Cargo.WebApi/Controllers/CargoOperationsController.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Cargo.BusinessLayer.Abstract;
+using MultiShop.Cargo.DtoLayer.CargoOperationDtos;
+using MultiShop.Cargo.EntityLayer.Concrete;
+
+namespace MultiShop.Cargo.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CargoOperationsController : ControllerBase
+    {
+        private readonly ICargoOperationService _cargoOperationService;
+        private readonly IMapper _mapper;
+
+        public CargoOperationsController(ICargoOperationService cargoOperationService, IMapper mapper)
+        {
+            _cargoOperationService = cargoOperationService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult CargoOperationList()
+        {
+            var values = _cargoOperationService.TGetAll();
+            return Ok(values);
+        }
+
+        [HttpGet]
+        public IActionResult GetCargoOperationById(int id)
+        {
+            var values = _cargoOperationService.TGetById(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCargoOperation(CreateCargoOperationDto createCargoOperationDto)
+        {
+            var values = _mapper.Map<CargoOperation>(createCargoOperationDto);
+            _cargoOperationService.TInsert(values);
+            return Ok("Cargo operation created successfully");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCargoOperation(UpdateCargoOperationDto updateCargoOperationDto)
+        {
+            var values = _mapper.Map<CargoOperation>(updateCargoOperationDto);
+            _cargoOperationService.TUpdate(values);
+            return Ok("Cargo operation updated successfully");
+        }
+
+        [HttpDelete]
+        public IActionResult RemoveCargoOperation(int id)
+        {
+            _cargoOperationService.TDelete(id);
+            return Ok("Cargo operation deleted successfully");
+        }
+    }
+}
