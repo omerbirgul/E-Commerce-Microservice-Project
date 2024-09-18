103c59e8faf35387c7e649b85205e6374bd19a7b
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2024-09-18 17:17:20 +0300

    admin panel's UI ProductlistWithCategory function added

--- MultiShop/Frontends/MultiShop.DtoLayer/CatalogDtos/ProductDtos/ResultProductWithCategoryDto.cs
+++ MultiShop/Frontends/MultiShop.DtoLayer/CatalogDtos/ProductDtos/ResultProductWithCategoryDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MultiShop.DtoLayer.CatalogDtos.CategoryDtos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,11 @@ namespace MultiShop.DtoLayer.CatalogDtos.ProductDtos
 {
     public class ResultProductWithCategoryDto
     {
+        public string ProductID { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public string ProductImageURL { get; set; }
+        public string ProductDescription { get; set; }
+        public ResultCategoryDto Category { get; set; }
     }
 }
--- MultiShop/Frontends/MultiShop.WebUI/Areas/Admin/Controllers/ProductController.cs
+++ MultiShop/Frontends/MultiShop.WebUI/Areas/Admin/Controllers/ProductController.cs
@@ -51,6 +51,21 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
         }
 
         [HttpGet]
+        [Route("ProductListWithCategory")]
+        public async Task<IActionResult> ProductListWithCategory()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:7071/api/Products/ProductListWithCategory");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductWithCategoryDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
         [Route("CreateProduct")]
         public async Task<IActionResult> CreateProduct()
         {
