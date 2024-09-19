8346f2f90a83f1fad61b649b1e3a0f4fa2567a5c
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2024-09-19 18:24:54 +0300

    admin panel categorySlide dtos, controller, index page added

---
+++ MultiShop/Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategorySlideController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.CategorySlideDtos;
+using Newtonsoft.Json;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/CategorySlide")]
+    [AllowAnonymous]
+    public class CategorySlideController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CategorySlideController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:7071/api/CategorySlides");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategorySlideDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
