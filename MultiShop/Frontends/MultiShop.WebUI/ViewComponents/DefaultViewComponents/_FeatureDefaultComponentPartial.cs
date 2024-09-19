ded588a035f78f08817921f10cfd29acb8736b56
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2024-09-19 23:43:26 +0300

    webUI featured productds connected to API and page get dynamiched

--- MultiShop/Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureDefaultComponentPartial.cs
+++ MultiShop/Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureDefaultComponentPartial.cs
@@ -1,9 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.ProductDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _FeatureDefaultComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke() { return View(); }
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _FeatureDefaultComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync() 
+        { 
+            var client  = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:7071/api/Products");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
     }
 }
