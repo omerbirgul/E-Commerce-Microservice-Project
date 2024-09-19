a16b8005d784e88b56a2f84ba24987c5284e63d5
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2024-09-19 19:51:03 +0300

    webUI categorySlides page dynamically added

--- MultiShop/Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_CategorySlideDefaultComponentPartial.cs
+++ MultiShop/Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_CategorySlideDefaultComponentPartial.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.CategorySlideDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _CategorySlideDefaultComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _CategorySlideDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:7071/api/CategorySlides");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategorySlideDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
