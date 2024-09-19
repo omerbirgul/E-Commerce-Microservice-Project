5f3fa45223f9b76c30ab47265afcc35c36e16ebc
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2024-09-19 19:21:30 +0300

    admin panel categorySlide controller delete method added

--- MultiShop/Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategorySlideController.cs
+++ MultiShop/Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategorySlideController.cs
@@ -35,7 +35,7 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
 
         [HttpGet]
         [Route("CreateCategorySlide")]
-        public async Task<IActionResult> CreateCategorySlide()
+        public IActionResult CreateCategorySlide()
         {
             return View();
         }
@@ -48,6 +48,48 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
             var jsonData = JsonConvert.SerializeObject(createCategorySlideDto);
             StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
             var responseMessage = await client.PostAsync("http://localhost:7071/api/CategorySlides", stringContent);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "CategorySlide", new { area = "Admin" });
+            }
+            return View();
+        }
+
+        [HttpGet]
+        [Route("UpdateCategorySlide/{id}")]
+        public async Task<IActionResult> UpdateCategorySlide(string id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:7071/api/CategorySlides/" + id);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<UpdateCategorySlideDto>(jsonData);
+                return View(value);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [Route("UpdateCategorySlide/{id}")]
+        public async Task<IActionResult> UpdateCategorySlide(UpdateCategorySlideDto updateCategorySlideDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateCategorySlideDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("http://localhost:7071/api/CategorySlides", stringContent);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "CategorySlide", new { area = "Admin" });
+            }
+            return View();
+        }
+
+        [Route("DeleteCategorySlide/{id}")]
+        public async Task<IActionResult> DeleteCategorySlide(string id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync("http://localhost:7071/api/CategorySlides/" + id);
             if(responseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index", "CategorySlide", new { area = "Admin" });
