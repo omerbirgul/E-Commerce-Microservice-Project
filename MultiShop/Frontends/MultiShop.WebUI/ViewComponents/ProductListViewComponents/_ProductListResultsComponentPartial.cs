22cfe849997c6c41f2944986819b8335bbd16e81
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2024-09-24 01:44:39 +0300

    webUI productListWithCategoryById page statically added and connected to API

--- MultiShop/Frontends/MultiShop.WebUI/ViewComponents/ProductListViewComponents/_ProductListResultsComponentPartial.cs
+++ MultiShop/Frontends/MultiShop.WebUI/ViewComponents/ProductListViewComponents/_ProductListResultsComponentPartial.cs
@@ -1,11 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.ProductDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.ProductListViewComponents
 {
     public class _ProductListResultsComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ProductListResultsComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string id)
+        {
+            id = "66ec87558e189f119b176bc1";
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client
+                .GetAsync("http://localhost:7071/api/Products/ProductsWithCategoryByCategoryId?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductWithCategoryDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
