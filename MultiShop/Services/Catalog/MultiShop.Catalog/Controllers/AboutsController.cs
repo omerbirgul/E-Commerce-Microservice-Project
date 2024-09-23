1a9c7e3b8d8cda88452fe8964e5a05b68251f6c4
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2024-09-23 16:28:05 +0300

    CatalogService aboutController added

---
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Controllers/AboutsController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.AboutDtos;
+using MultiShop.Catalog.Services.AboutServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class AboutsController : ControllerBase
+    {
+        private readonly IAboutService _aboutService;
+
+        public AboutsController(IAboutService aboutService)
+        {
+            _aboutService = aboutService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AboutsList()
+        {
+            var values = await _aboutService.AboutListAsync();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAbout(CreateAboutDto createAboutDto)
+        {
+            await _aboutService.CreateAboutAsync(createAboutDto);
+            return Ok("About created successfully");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAbout(UpdateAboutDto updateAboutDto)
+        {
+            await _aboutService.UpdateAboutAsync(updateAboutDto);
+            return Ok("About updated successfully");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAbout(string id)
+        {
+            await _aboutService.DeleteAboutAsync(id);
+            return Ok("About deleted successfully");
+        }
+    }
+}
