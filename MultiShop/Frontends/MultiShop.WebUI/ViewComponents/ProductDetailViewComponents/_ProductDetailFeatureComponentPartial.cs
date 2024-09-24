30d07f88e05d7bd3c155688ef4151c3021ee124f
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2024-09-24 14:43:34 +0300

    webUI productDetail gape routing added

--- MultiShop/Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponents/_ProductDetailFeatureComponentPartial.cs
+++ MultiShop/Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponents/_ProductDetailFeatureComponentPartial.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.ProductDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.ProductDetailViewComponents
 {
     public class _ProductDetailFeatureComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ProductDetailFeatureComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:7071/api/Products/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<UpdateProductDto>(jsonData);
+                return View(value);
+            }
             return View();
         }
     }
