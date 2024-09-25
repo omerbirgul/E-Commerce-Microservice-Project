1fbe2f7ad23d0e32ad10efd90f43b98283a4021f
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2024-09-26 00:01:55 +0300

    CommentService added

---
+++ MultiShop/Services/Comment/MultiShop.Comment/Entities/UserComment.cs
@@ -0,0 +1,14 @@
+﻿namespace MultiShop.Comment.Entities
+{
+    public class UserComment
+    {
+        public string UserCommentId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string? ImageUrl { get; set; }
+        public string CommentDetail { get; set; }
+        public int Rating { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool Status { get; set; }
+    }
+}
