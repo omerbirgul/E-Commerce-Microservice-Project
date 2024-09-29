02d5b36d5d8ead57960a9ac2ebbfe0e385689ce3
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2024-09-29 14:53:21 +0300

    CatalogService contactCollection added

--- MultiShop/Services/Catalog/MultiShop.Catalog/Settings/DatabaseSettings.cs
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Settings/DatabaseSettings.cs
@@ -7,6 +7,7 @@
         public string ProductDetailCollectionName { get; set; }
         public string ProductImageCollectionName { get; set; }
         public string CategorySlideCollectionName { get; set; }
+        public string ContactCollectionName { get; set; }
         public string BrandCollectionName { get; set; }
         public string AboutCollectionName { get; set; }
         public string ConnectionString { get; set; }
