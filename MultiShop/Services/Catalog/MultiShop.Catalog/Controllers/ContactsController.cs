7ffbc4de8a5d74c20f4dcc8cc4e1824e7a16e0c7
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2024-09-29 15:12:19 +0300

    CatalogService comment API controller added

---
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Controllers/ContactsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.ContactDtos;
+using MultiShop.Catalog.Services.ContactServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class ContactsController : ControllerBase
+    {
+        private readonly IContactService _contactService;
+
+        public ContactsController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllContacts()
+        {
+            var values = await _contactService.GetAllContactAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetContactById(string id)
+        {
+            var value = await _contactService.GetByIdContactAsync(id);
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateContact(CreateContactDto createContactDto)
+        {
+            await _contactService.CreateContactAsync(createContactDto);
+            return Ok("Contact added successfully");
+        }
+    }
+}
