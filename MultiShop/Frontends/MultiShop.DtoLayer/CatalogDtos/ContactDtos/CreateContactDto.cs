b4c7dab4d54978fd434c27cc82f51681229622a3
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2024-09-29 15:22:54 +0300

    webUI contactDtos added

---
+++ MultiShop/Frontends/MultiShop.DtoLayer/CatalogDtos/ContactDtos/CreateContactDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.DtoLayer.CatalogDtos.ContactDtos
+{
+    public class CreateContactDto
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+        public bool IsRead { get; set; }
+        public DateTime SendDate { get; set; }
+    }
+}
