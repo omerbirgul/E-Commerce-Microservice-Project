af54320985301a1b52dc9894237c7b3358a09429
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2024-10-01 17:11:38 +0300

    fix typo

--- MultiShop/IdentityServer/MultiShop.IdentityServer/Controllers/LoginsController.cs
+++ MultiShop/IdentityServer/MultiShop.IdentityServer/Controllers/LoginsController.cs
@@ -13,22 +13,29 @@ namespace MultiShop.IdentityServer.Controllers
     public class LoginsController : ControllerBase
     {
         private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public LoginsController(SignInManager<ApplicationUser> signInManager)
+        public LoginsController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
         {
             _signInManager = signInManager;
+            _userManager = userManager;
         }
 
         [HttpPost]
         public async Task<IActionResult> UserLogin(UserLoginDto userLoginDto)
         {
-            var result = await _signInManager
-                .PasswordSignInAsync(userLoginDto.Username, userLoginDto.Password, false, false);
+            var result = await _signInManager.PasswordSignInAsync(
+                userLoginDto.Username,
+                userLoginDto.Password,
+                false,
+                false);
+
+            var user = await _userManager.FindByNameAsync(userLoginDto.Username);
             if(result.Succeeded)
             {
                 GetCheckAppUserViewModel model = new GetCheckAppUserViewModel();
                 model.Username = userLoginDto.Username;
-                model.Id = "1";
+                model.Id = user.Id;
                 var token = JwtTokenGenerator.GenerateToken(model);
                 return Ok(token);
             }
