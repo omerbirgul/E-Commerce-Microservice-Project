8636974083106c6ad455fca88061c821526b5474
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2024-10-02 23:20:55 +0300

    ClientSettings added

---
+++ MultiShop/Frontends/MultiShop.WebUI/Services/Concrete/IdentityService.cs
@@ -0,0 +1,13 @@
+﻿using MultiShop.DtoLayer.IdentityDtos.LoginDtos;
+using MultiShop.WebUI.Services.Abstract;
+
+namespace MultiShop.WebUI.Services.Concrete
+{
+    public class IdentityService : IIdentityService
+    {
+        public Task<bool> SignIn(SignInDto signInDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MultiShop/Frontends/MultiShop.WebUI/Settings/ClientSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiShop.WebUI.Settings
+{
+    public class ClientSettings
+    {
+        public Client MultiShopVisitorClient { get; set; }
+        public Client MultiShopManagerClient { get; set; }
+        public Client MultiShopAdminClient { get; set; }
+    }
+}
