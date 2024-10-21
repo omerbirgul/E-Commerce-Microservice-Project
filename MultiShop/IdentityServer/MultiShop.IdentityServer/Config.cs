ffe806b0672db04233cb68b49c7ebca0ffd20897
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2024-10-21 23:31:30 +0300

    webUI Discount services added

--- MultiShop/IdentityServer/MultiShop.IdentityServer/Config.cs
+++ MultiShop/IdentityServer/MultiShop.IdentityServer/Config.cs
@@ -65,7 +65,7 @@ namespace MultiShop.IdentityServer
                 AllowedGrantTypes= GrantTypes.ResourceOwnerPassword,
                 ClientSecrets = {new Secret("multishopsecret".Sha256())},
                 AllowedScopes = {"CatalogReadPermission", "CatalogFullPermission", "BasketFullPermission",
-                    "OcelotFullPermission", "CommentFullPermission",
+                    "OcelotFullPermission", "CommentFullPermission", "DiscountFullPermission",
                 IdentityServerConstants.LocalApi.ScopeName,
                 IdentityServerConstants.StandardScopes.Email,
                 IdentityServerConstants.StandardScopes.OpenId,
