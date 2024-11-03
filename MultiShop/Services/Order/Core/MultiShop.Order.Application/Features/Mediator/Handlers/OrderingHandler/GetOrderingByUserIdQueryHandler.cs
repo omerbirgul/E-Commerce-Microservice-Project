0f28ef0b3f1327ed86fdb687a55857043ea17172
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2024-11-03 14:13:24 +0300

    services/ OrderService order query and result classes added

---
+++ MultiShop/Services/Order/Core/MultiShop.Order.Application/Features/Mediator/Handlers/OrderingHandler/GetOrderingByUserIdQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using MultiShop.Order.Application.Features.Mediator.Queries.OrderingQueries;
+using MultiShop.Order.Application.Features.Mediator.Results.OrderingResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.Mediator.Handlers.OrderingHandler
+{
+    public class GetOrderingByUserIdQueryHandler : IRequestHandler<GetOrderingByUserIdQuery, 
+        List<GetOrderingByUserIdQueryResult>>
+    {
+        public Task<List<GetOrderingByUserIdQueryResult>> Handle(GetOrderingByUserIdQuery request, 
+            CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
