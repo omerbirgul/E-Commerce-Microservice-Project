c1df998e8a0f9c89e28429d747ff08667d200f8d
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2024-11-03 17:55:21 +0300

    webUI/UserArea/ OrderController routing added

--- MultiShop/Frontends/MultiShop.WebUI/Areas/User/Controllers/MyOrderController.cs
+++ MultiShop/Frontends/MultiShop.WebUI/Areas/User/Controllers/MyOrderController.cs
@@ -1,13 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.WebUI.Services.Abstract;
+using MultiShop.WebUI.Services.OrderServices.OrderingServices;
 
 namespace MultiShop.WebUI.Areas.User.Controllers
 {
     [Area("User")]
     public class MyOrderController : Controller
     {
-        public IActionResult Index()
+        private readonly IOrderingService _orderingService;
+        private readonly IUserService _userService;
+
+        public MyOrderController(IOrderingService orderingService, IUserService userService)
         {
-            return View();
+            _orderingService = orderingService;
+            _userService = userService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userService.GetUserInfoAsync();
+            var orderingValues = await _orderingService.GetOrderingByUserIdAsync(user.Id);
+            return View(orderingValues);
         }
     }
 }
