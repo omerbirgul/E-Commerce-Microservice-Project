b8ae6a31c6c695d82af405c2d6472b3e2ec8d40a
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2024-11-05 23:08:38 +0300

    services/MessageService/ add entity props

--- MultiShop/Services/Message/MultiShop.Message/DAL/Entities/UserMessage.cs
+++ MultiShop/Services/Message/MultiShop.Message/DAL/Entities/UserMessage.cs
@@ -2,5 +2,12 @@
 {
     public class UserMessage
     {
+        public int UserMessageId { get; set; }
+        public string SenderId { get; set; }
+        public string ReceiverId { get; set; }
+        public string Subject { get; set; }
+        public string MessageDetail { get; set; }
+        public DateTime MessageDate { get; set; }
+        public bool IsRead { get; set; }
     }
 }
